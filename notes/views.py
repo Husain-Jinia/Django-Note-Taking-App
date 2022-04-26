48de91cb9e066de6797b7021bbc1baf31bbb8847
Husain-Jinia <husain.jinia0201@gmail.com>
2022-04-26 21:38:26 +0530

    [Added] create note func

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import  CreateView,DeleteView, UpdateView, DetailView
+from models import *
 
 # Create your views here.
+class CreateNotes(CreateView):
+    model = Notes
+    template_name = 'create-notes.html'
